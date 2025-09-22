cfd964dbd102a49ecf70228edcb5ce5f352d5d52
ubeslug <136268381+ubeslug@users.noreply.github.com>
2025-09-22 12:23:13 -0700

    tweaks

--- scripts/performer.gd
+++ scripts/performer.gd
@@ -5,7 +5,8 @@ class_name Performer
 var performer_gravity: float = 400
 
 func _process(delta):
-	$CanvasLayer/velocity.text = "Velocity: " + str(self.velocity)
+		pass
+		#$CanvasLayer/velocity.text = "Velocity: " + str(self.velocity)
 
 func _physics_process(delta):
 	pass
